6b0162c2a17730a82cad7a05b97cb1f306969b31
Henrique Afonso <henrique8afonso@gmail.com>
2021-06-14 03:43:13 +0100

    improved stuff need to click with every title now

--- meh.py
+++ meh.py
@@ -2,26 +2,36 @@ from selenium import webdriver
 from selenium.webdriver import Opera
 from selenium.webdriver.chrome.webdriver import WebDriver
 import requests
-
-
-
-
+from bs4 import BeautifulSoup
+import urllib.request
 
 
 
 driver = webdriver.Opera(executable_path="C:\WebDriver\operadriver.exe")
 
-
-url="https://www.pcdiga.com/placa-grafica-gigabyte-geforce-rtx-3090-aorus-gaming-box-24gb-gv-n3090ixeb-24gd-1-0?search=3090"
+url="https://www.pcdiga.com/#/embedded/query=3090&page=1&filter%5Bcategories%5D%5B0%5D=Placas%20Gráficas&query_name=match_and"
 
 driver.get(url)
-
 driver.maximize_window()
+driver.implicitly_wait(40)
+
+a=driver.find_elements_by_class_name("df-card")
+
+
+b=[]
+for j in a:
+	b.append(j.text)
+
+c=[]
+
+for i in b:
+	if "Sem Stock" in i:
+		continue
+	else:
+		c.append(i.split('\n')[0])
 
 
 
-click = driver.find_element_by_xpath("/html/body/div[3]/main/div[3]/div/div[2]/div[4]")
-driver.maximize_window()
-
-click.click()
-
+for i in c:
+	l=driver.find_element_by_xpath("//span[.="\i""]")
+	l.click()
